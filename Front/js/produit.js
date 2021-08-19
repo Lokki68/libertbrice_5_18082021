10d287555b622b79c9cbec1a40a812c4b0fa654b
Brice Libert <bricel67@gmaiol.com>
2021-08-19 18:29:00 +0200

    product.js

--- Front/js/produit.js
+++ Front/js/produit.js
@@ -16,31 +16,23 @@ fetch(urlTeddy)
         // passage du prix en €
         let price = data.price / 100
 
-      document.querySelector('.card_product').innerHTML = `
-      <div class="card_product-name">
-      ${data.name}
-    </div>
-    <div class="card_product-img">
-      <!-- implémenté par Js -->
-      <img src="${data.imageUrl}" alt="Photo ${data.name}">
-    </div>
-    <div class="card_product-description">
-      <!-- implémenté par Js -->
-      <p>${data.description}</p>
-      <p class="prix"> ${price} €</p>
-    </div>
-    <div class="option_achat">
-    <label for="select_color"> Couleur : </label>
-    <select id="select_color">
-      
-    </select>
-    <form class="form">
-      <label for="nombre">Nb :</label>
-      <input type="number" id="nombre" name="name" required min="1" max="10" size="10">
-    </form>
-    <button class="btn validate" data-id="test" type="submit">C'est toi que je veux</button>
-    </div>
-      `  
-        })
+        // Insertion Name
+      document.querySelector('.card_product-name').innerHTML = `<h3>${data.name}</h3> `  
+        
+        // Insertion Photo  URL
+        document.querySelector('.card_product-img').innerHTML = `<img src="${data.imageUrl}" alt= "Photo ${data.name}">`
+
+        // Insertion Description + Prix
+        document.querySelector('.card_product-description').innerHTML = `
+        <p>${data.description}</p>
+        <p class="prix">${price} €</p>`
+        
+
+        for (let i = 0; i < data.colors.length; i++) {
+          console.log(data.colors[i])
+          document.querySelector('#select_color').innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`
+        }
+        
+      })
   )
   .catch((error) => console.log('erreur : ' + error));
