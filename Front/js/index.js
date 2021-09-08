01a45b948be287035f425a10dfdd40fed6e0d737
Brice <bricel67@gmail.com>
2021-09-08 16:51:23 +0200

    Nettoyage index.js
    Rajout des fonctions

--- Front/js/index.js
+++ Front/js/index.js
@@ -1,39 +1,48 @@
-// Récupération de l'url pour la demande sur le backend
 const urlTeddies = 'http://localhost:3000/api/teddies';
 
-// Demande des informations au backend
-fetch(urlTeddies)
-  .then(response =>
-    response.json()
-      .then(data => {
 
-        // Affichage de tous les oursons
-        data.forEach(teddie => {
+main()
 
-          // Récupération de l'id = presentations pour affichage sur la page html des oursons
-      document.querySelector('#presentations').innerHTML += `
-      <div class="cards"> 
+async function main() {
+  const Teddies = await getTeddies()
+  
+  Teddies.forEach(teddy => {
+    displayTeddies(teddy)
+  })
+}
+
+
+function getTeddies() {
+  return fetch(urlTeddies)
+  .then((response) => {
+    return response.json()
+  })
+  .then(function (teddies) {
+    return teddies
+  })
+  .catch((error) => {
+    document.querySelector('#notification').innerHTML = `
+    <div class="notification_alert" role="alert">
+      ${error}
+    </div>`
+  })
+}
+
+
+function displayTeddies(teddy) {
+  document.querySelector('#presentations').innerHTML += `
+    <div class="cards"> 
       <div class="card-img">
-          <img src="${teddie.imageUrl}" alt="${teddie.name}">
+        <img src="${teddy.imageUrl}" alt="${teddy.name}">
+      </div>
+      <div class="card-description">
+        <div class="description-name">
+          <h3>${teddy.name}</h3>
         </div>
-        <div class="card-description">
-          <div class="description-name">
-            <h3>${teddie.name}</h3>
-          </div>
-          <div class="description-discovert">
-          <a href="produit.html?id=${teddie._id}"><button  class="btn">Découvre moi</button></a>
-          </div>
+        <div class="description-discovert">
+          <a href="produit.html?id=${teddy._id}"><button  class="btn">Découvre moi</button></a>
         </div>
-        </div>
-      `  
-        });
-      })
-  )
-  .catch((error) => 
-  document.querySelector('#notification').innerHTML = `
-  <div class="notification_alert" role="alert">
-          ${error}
-        </div>
-  `
-  );
-
+      </div>
+    </div>
+  `  
+};
